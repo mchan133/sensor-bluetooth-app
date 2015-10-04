63c29783a815c904ac87bf18c1f73960a93ff93e
Matt Chan <mchan133@gmail.com>
2015-10-04 19:45:36 -0400

    Added basic partial layout, second activity

--- app/src/main/java/com/nanobiotechru/android/nanobiotechandroid/SecondActivity.java
+++ app/src/main/java/com/nanobiotechru/android/nanobiotechandroid/SecondActivity.java
@@ -1,25 +1,23 @@
 package com.nanobiotechru.android.nanobiotechandroid;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
 
-
-public class MainActivity extends ActionBarActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second, menu);
         return true;
     }
 
@@ -38,8 +36,15 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onConnectClick(View view){
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+    //Goes to data collection
+    public void onCollectDataClick(View view){
+    }
+
+    //Goes to details of most recent result
+    public void onRecentResultClick(View view){
+    }
+
+    //Goes to list of past results
+    public void onResultHistoryClick(View view){
     }
 }
